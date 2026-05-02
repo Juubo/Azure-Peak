ShadowLarkens/Ochre-Valley

Quest 2 + Steward 2 + Economy 2 + Taxation 2 + Law 2 + Innkeeper 2 (#6849)
* Implement Party Mechanics as Fellowship

* Remove Beacon Quest

* Initial Commit for Quest Generation system

* Pool replenishment adjustment

* Anti-Abandonment, Quest Logging and Statistics

* Defer quest generation

* Update contract examine

* Fix le compile issue

* Redo ContractLedger interface

* Redo contract ledger distance calculation

* Uses human adjacent state for contract ledger

* Commit Steward quest module plan.

* Update Module 2

* Add Per Region marker

* Regional sorting for Contracts

* Initial work for factions.

* Quest take cooldown and Factions / Bounty REfactor

* Boss name refactor

* Makes Quest Generation consumes factions and uses factions.

* Add Underdark and Decap test region

* IC Ambush Threat level translations and actually initialize factions

* Quest Reward rework with tp scaled formula.

* Add Coast to roguetest.

* Quest Balance: Mob TP Floors, reward tuning and minimum mob floor.

* Fellowship Gate Raid & Bounty Quest

* Threat rebalance and content depth increase

* Add drow faction to dme.

* Retrieval and Recovery Quest overhaul.

* Remove legacy TG Economy system

* Refactor of banking system to use account

* Politics stub

* Clear leftover TG ref

* Update Contract INterface

* Kills Interest mechnaics.,

* Bank Account to Fund refactor

* Rework all call sites for treasury to mint, burn and transfer

* Rework Tax Category

* Tax setting and old per category tax obliteration

* Add in the decree system.

* Revoke / Restore granting text.

* Charters, lord panel, resident letters and fine / payroll polishes.

* Migrate initial account creation

* Outlaw are no longer tax or fine exempt

* Fix Declare Outlaw

* Grand Duke to 250

* Patronage mechanics

* War Chest Tick

* Remove safeguard

* Walk of shame for being insolvent

* Refactor and redo Benefactor code flavor.

* Loan mechanics

* Burgher Bond from war chest

* Debt and Debtor Fixes

* 

code/modules/roguetown/roguemachine/stockpile/stockpile.dm
@@ -6,9 +6,8 @@
 	density = FALSE
 	blade_dulling = DULLING_BASH
 	pixel_y = 32
-	var/stockpile_index = 1
 	var/current_category = "Raw Materials"
-	var/list/categories = list("Raw Materials", "Fruit", "Vegetable", "Animal","Seafood")
+	var/list/categories = list("Raw Materials", "Refined", "Alchemy", "Fruit", "Vegetable", "Animal", "Seafood", "Precious")
 	var/datum/withdraw_tab/withdraw_tab = null
 
 /obj/structure/roguemachine/stockpile/get_mechanics_examine(mob/user)
@@ -22,7 +21,7 @@
 /obj/structure/roguemachine/stockpile/Initialize()
 	. = ..()
 	SSroguemachine.stock_machines += src
-	withdraw_tab = new(stockpile_index, src)
+	withdraw_tab = new(src)
 
 
 /obj/structure/roguemachine/stockpile/Destroy()
@@ -88,7 +87,11 @@
 	for(var/datum/roguestock/stockpile/R in SStreasury.stockpile_datums)
 		if(R.category != current_category)
 			continue
-		contents += "[R.name] - [R.payout_price] - ([R.held_items[stockpile_index]]/[R.stockpile_limit]) - [R.demand2word()]"
+		R.refresh_auto_price()
+		if(!R.accept_toggle_enabled)
+			contents += "<font color='#888'>[R.name][R.get_event_tag()] - NOT ACCEPTING - ([R.stockpile_amount]/[R.stockpile_limit])</font>"
+		else
+			contents += "[R.name][R.get_event_tag()] - [R.payout_price][R.get_market_delta_tag_for("deposit")] - ([R.stockpile_amount]/[R.stockpile_limit])"
 		contents += "<BR>"
 
 	return contents
@@ -149,60 +152,124 @@
 			playsound(loc, 'sound/misc/hiss.ogg', 100, FALSE, -1)
 		return
 
+	// Pre-check: farmer must have a Meister account. Otherwise the stockpile would silently
+	// eat their goods for no payment - do not scam walk-ins.
+	var/has_account = SStreasury.has_account(H)
+	if(!has_account)
+		if(message)
+			say("No account found for [H]. Submit your fingers to a Meister for inspection.")
+		return
+
+	// Pre-check: Crown's Purse must be solvent enough to pay. Below the Steward-set floor,
+	// the Crown refuses purchases entirely - goods stay in the farmer's hands.
+	var/treasury_balance = SStreasury.discretionary_fund?.balance || 0
+	var/below_floor = treasury_balance < SStreasury.stockpile_purchase_floor
+
 	for(var/datum/roguestock/R in SStreasury.stockpile_datums)
 		if(istype(I, /obj/item/natural/bundle))
 			var/obj/item/natural/bundle/B = I
 			if(B.stacktype == R.item_type)
-				var/nopay = R.held_items[stockpile_index] >= R.stockpile_limit // Check whether it is overflowed BEFORE nopaying them
-				R.held_items[stockpile_index] += B.amount
+				if(!R.accept_toggle_enabled)
+					if(message)
+						say("The Crown has no interest in [R.name] at this time.")
+					return
+				if(below_floor && !R.mint_item)
+					if(message)
+						say("The Crown's ledger is thin. No purchases today.")
+					return
+				if(R.stockpile_amount >= R.stockpile_limit)
+					if(message)
+						say("The Crown's [R.name] stockpile is full. Take it elsewhere.")
+					return
+				var/bundle_amt = B.amount
+				R.stockpile_amount += bundle_amt
 				if(message == TRUE)
-					stock_announce("[B.amount] units of [R.name] has been stockpiled.")
+					stock_announce("[bundle_amt] units of [R.name] has been stockpiled.")
 				qdel(B)
 				if(sound == TRUE)
 					playsound(loc, 'sound/misc/hiss.ogg', 100, FALSE, -1)
-				if(nopay)
-					SStreasury.economic_output += R.export_price * B.amount // Still count
-					say("Stockpile is full, no payment.")
-				else
-					var/amt = R.payout_price * B.amount
-					SStreasury.economic_output += R.export_price * B.amount
-					if(!SStreasury.give_money_account(amt, H, "+[amt] from [R.name] bounty") && message == TRUE)
-						say("No account found. Submit your fingers to a Meister for inspection.")
-					else
-						record_round_statistic(STATS_STOCKPILE_EXPANSES, amt)
+				R.refresh_auto_price()
+				var/per_unit = R.payout_price
+				var/amt = per_unit * bundle_amt
+				SStreasury.economic_output += amt
+				SStreasury.give_money_account(amt, H, "+[amt] from [R.name] bounty")
+				record_round_statistic(STATS_STOCKPILE_EXPANSES, amt)
+				return
 			continue
 		// Bloc to replace old vault mechanics
 		else if(istype(I,R.item_type))
 			if(!R.check_item(I))
 				continue
+			if(R.mint_item && I.unmintable)
+				if(message)
+					say("This is town property, it cannot be minted here.")
+				return
+			// Steward-controlled accept toggle.
+			// - For mint_eligible goods (gems): falls through to the /bounty/treasure datum later in
+			//   the loop, so rejected gems still mint as treasure instead of bouncing back to the player.
+			// - For other goods (raw, refined, alchemy): refuses with a message. Item stays in hand.
+			if(!R.accept_toggle_enabled)
+				var/datum/trade_good/tg_reject = R.trade_good_id ? GLOB.trade_goods[R.trade_good_id] : null
+				if(tg_reject && tg_reject.mint_eligible)
+					continue
+				if(message)
+					say("The Crown has no interest in [R.name] at this time.")
+				return
+			// Treasure / mint items bypass the purchase floor - they generate mammon rather than spending it.
+			if(below_floor && !R.mint_item)
+				if(message)
+					say("The Crown's ledger is thin. No purchases today.")
+				return
+			// Trade-good overflow mint branch. If this entry is linked to a mint_eligible
+			// trade good (gems) and the stockpile is at limit, overflow mints to Crown's Purse
+			// using the existing treasure-mint path. Takes precedence over no-pay overflow.
+			if(R.trade_good_id && !R.mint_item && R.stockpile_amount >= R.stockpile_limit)
+				var/datum/trade_good/tg_overflow = GLOB.trade_goods[R.trade_good_id]
+				if(tg_overflow && tg_overflow.mint_eligible)
+					var/mint_amt = round(tg_overflow.base_price * SStreasury.mint_multiplier)
+					SStreasury.minted += mint_amt
+					SStreasury.mint(SStreasury.discretionary_fund, mint_amt, "Gem overflow mint: [tg_overflow.name]")
+					record_round_statistic(STATS_MINTED_TREASURE_GROSS, mint_amt)
+					qdel(I)
+					if(sound == TRUE)
+						playsound(loc, 'sound/misc/hiss.ogg', 100, FALSE, -1)
+						playsound(loc, 'sound/misc/disposalflush.ogg', 100, FALSE, -1)
+					say("[tg_overflow.name] overflow - minted to Crown's Purse.")
+					return
+			if(!R.mint_item && R.stockpile_amount >= R.stockpile_limit)
+				if(message)
+					say("The Crown's [R.name] stockpile is full. Take it elsewhere.")
+				return
+			R.refresh_auto_price()
 			var/amt = R.get_payout_price(I)
-			var/nopay = !R.mint_item && R.held_items[stockpile_index] >= R.stockpile_limit // Check whether it is overflowed BEFORE nopaying them
+			var/true_value = I.get_real_price()
 			if(!R.mint_item)
-				R.held_items[stockpile_index] += 1 //stacked logs need to check for multiple
+				R.stockpile_amount += 1 //stacked logs need to check for multiple
 				qdel(I)
 				if(message == TRUE)
 					stock_announce("[R.name] has been stockpiled.")
 				if(sound == TRUE)
 					playsound(loc, 'sound/misc/hiss.ogg', 100, FALSE, -1)
 			else
-				var/mint_amt = round(SStreasury.mint_multiplier * I.get_real_price())
+				var/mint_amt = round(SStreasury.mint_multiplier * true_value)
 				SStreasury.minted += mint_amt
-				SStreasury.give_money_treasury(mint_amt, "Minting - [I.name]", FALSE)
+				SStreasury.mint(SStreasury.discretionary_fund, mint_amt, "Minting - [I.name]")
+				record_round_statistic(STATS_MINTED_TREASURE_GROSS, mint_amt)
+				record_round_statistic(STATS_MINTED_TREASURE_NET, max(0, mint_amt - amt))
 				qdel(I) // Eaten to be minted!
 				if(sound == TRUE)
 					playsound(loc, 'sound/misc/hiss.ogg', 100, FALSE, -1)
 					playsound(loc, 'sound/misc/disposalflush.ogg', 100, FALSE, -1)
-			var/true_value = I.get_real_price()
-			if(nopay)
-				SStreasury.economic_output += true_value // Still count as economic output hah
-				say("Stockpile is full, no payment.")
-			else if(amt)
+			if(amt)
 				SStreasury.economic_output += true_value
-				if(!SStreasury.give_money_account(amt, H, "+[amt] from [R.name] bounty") && message == TRUE)
-					say("No account found. Submit your fingers to a Meister for inspection.")
+				SStreasury.give_money_account(amt, H, "+[amt] from [R.name] bounty")
 			record_round_statistic(STATS_STOCKPILE_EXPANSES, amt) // Unlike deposit, a treasure minting is equal to both expending and profiting at the same time
 			record_round_statistic(STATS_STOCKPILE_REVENUE, true_value)
 			return
+
+	// Nothing in the stockpile accepted this item
+	if(message)
+		say("[I.name] is not accepted here.")
 
 /obj/structure/roguemachine/stockpile/attackby(obj/item/P, mob/user, params)
 	if(istype(P, /obj/item/roguecoin/aalloy))

Juubo/Azure-Peak

code/modules/roguetown/roguemachine/stockpile/stockpile.dm
@@ -6,9 +6,8 @@
 	density = FALSE
 	blade_dulling = DULLING_BASH
 	pixel_y = 32
-	var/stockpile_index = 1
 	var/current_category = "Raw Materials"
-	var/list/categories = list("Raw Materials", "Fruit", "Vegetable", "Animal","Seafood")
+	var/list/categories = list("Raw Materials", "Refined", "Alchemy", "Fruit", "Vegetable", "Animal", "Seafood", "Precious")
 	var/datum/withdraw_tab/withdraw_tab = null
 
 /obj/structure/roguemachine/stockpile/get_mechanics_examine(mob/user)
@@ -22,7 +21,7 @@
 /obj/structure/roguemachine/stockpile/Initialize()
 	. = ..()
 	SSroguemachine.stock_machines += src
-	withdraw_tab = new(stockpile_index, src)
+	withdraw_tab = new(src)
 
 
 /obj/structure/roguemachine/stockpile/Destroy()
@@ -88,7 +87,11 @@
 	for(var/datum/roguestock/stockpile/R in SStreasury.stockpile_datums)
 		if(R.category != current_category)
 			continue
-		contents += "[R.name] - [R.payout_price] - ([R.held_items[stockpile_index]]/[R.stockpile_limit]) - [R.demand2word()]"
+		R.refresh_auto_price()
+		if(!R.accept_toggle_enabled)
+			contents += "<font color='#888'>[R.name][R.get_event_tag()] - NOT ACCEPTING - ([R.stockpile_amount]/[R.stockpile_limit])</font>"
+		else
+			contents += "[R.name][R.get_event_tag()] - [R.payout_price][R.get_market_delta_tag_for("deposit")] - ([R.stockpile_amount]/[R.stockpile_limit])"
 		contents += "<BR>"
 
 	return contents
@@ -149,35 +152,98 @@
 			playsound(loc, 'sound/misc/hiss.ogg', 100, FALSE, -1)
 		return
 
+	// Pre-check: farmer must have a Meister account. Otherwise the stockpile would silently
+	// eat their goods for no payment - do not scam walk-ins.
+	var/has_account = SStreasury.has_account(H)
+	if(!has_account)
+		if(message)
+			say("No account found for [H]. Submit your fingers to a Meister for inspection.")
+		return
+
+	// Pre-check: Crown's Purse must be solvent enough to pay. Below the Steward-set floor,
+	// the Crown refuses purchases entirely - goods stay in the farmer's hands.
+	var/treasury_balance = SStreasury.discretionary_fund?.balance || 0
+	var/below_floor = treasury_balance < SStreasury.stockpile_purchase_floor
+
 	for(var/datum/roguestock/R in SStreasury.stockpile_datums)
 		if(istype(I, /obj/item/natural/bundle))
 			var/obj/item/natural/bundle/B = I
 			if(B.stacktype == R.item_type)
-				var/nopay = R.held_items[stockpile_index] >= R.stockpile_limit // Check whether it is overflowed BEFORE nopaying them
-				R.held_items[stockpile_index] += B.amount
+				if(!R.accept_toggle_enabled)
+					if(message)
+						say("The Crown has no interest in [R.name] at this time.")
+					return
+				if(below_floor && !R.transport_item)
+					if(message)
+						say("The Crown's ledger is thin. No purchases today.")
+					return
+				if(R.stockpile_amount >= R.stockpile_limit)
+					if(message)
+						say("The Crown's [R.name] stockpile is full. Take it elsewhere.")
+					return
+				var/bundle_amt = B.amount
+				R.stockpile_amount += bundle_amt
 				if(message == TRUE)
-					stock_announce("[B.amount] units of [R.name] has been stockpiled.")
+					stock_announce("[bundle_amt] units of [R.name] has been stockpiled.")
 				qdel(B)
 				if(sound == TRUE)
 					playsound(loc, 'sound/misc/hiss.ogg', 100, FALSE, -1)
-				if(nopay)
-					SStreasury.economic_output += R.export_price * B.amount // Still count
-					say("Stockpile is full, no payment.")
-				else
-					var/amt = R.payout_price * B.amount
-					SStreasury.economic_output += R.export_price * B.amount
-					if(!SStreasury.give_money_account(amt, H, "+[amt] from [R.name] bounty") && message == TRUE)
-						say("No account found. Submit your fingers to a Meister for inspection.")
-					else
-						record_round_statistic(STATS_STOCKPILE_EXPANSES, amt)
+				R.refresh_auto_price()
+				var/per_unit = R.payout_price
+				var/amt = per_unit * bundle_amt
+				SStreasury.economic_output += amt
+				SStreasury.give_money_account(amt, H, "+[amt] from [R.name] bounty")
+				record_round_statistic(STATS_STOCKPILE_EXPANSES, amt)
+				return
 			continue
 		else if(istype(I,R.item_type))
 			if(!R.check_item(I))
 				continue
+			if(R.transport_item && I.unmintable)
+				if(message)
+					say("This is town property, it cannot be minted here.")
+				return
+			// Steward-controlled accept toggle.
+			// - For mint_eligible goods (gems): falls through to the /bounty/treasure datum later in
+			//   the loop, so rejected gems still mint as treasure instead of bouncing back to the player.
+			// - For other goods (raw, refined, alchemy): refuses with a message. Item stays in hand.
+			if(!R.accept_toggle_enabled)
+				var/datum/trade_good/tg_reject = R.trade_good_id ? GLOB.trade_goods[R.trade_good_id] : null
+				if(tg_reject && tg_reject.mint_eligible)
+					continue
+				if(message)
+					say("The Crown has no interest in [R.name] at this time.")
+				return
+			// Treasure / mint items bypass the purchase floor - they generate mammon rather than spending it.
+			if(below_floor && !R.transport_item)
+				if(message)
+					say("The Crown's ledger is thin. No purchases today.")
+				return
+			// Trade-good overflow mint branch. If this entry is linked to a mint_eligible
+			// trade good (gems) and the stockpile is at limit, overflow mints to Crown's Purse
+			// using the existing treasure-mint path. Takes precedence over no-pay overflow.
+			if(R.trade_good_id && !R.transport_item && R.stockpile_amount >= R.stockpile_limit)
+				var/datum/trade_good/tg_overflow = GLOB.trade_goods[R.trade_good_id]
+				if(tg_overflow && tg_overflow.mint_eligible)
+					var/mint_amt = round(tg_overflow.base_price * SStreasury.mint_multiplier)
+					SStreasury.minted += mint_amt
+					SStreasury.mint(SStreasury.discretionary_fund, mint_amt, "Gem overflow mint: [tg_overflow.name]")
+					record_round_statistic(STATS_MINTED_TREASURE_GROSS, mint_amt)
+					qdel(I)
+					if(sound == TRUE)
+						playsound(loc, 'sound/misc/hiss.ogg', 100, FALSE, -1)
+						playsound(loc, 'sound/misc/disposalflush.ogg', 100, FALSE, -1)
+					say("[tg_overflow.name] overflow - minted to Crown's Purse.")
+					return
+			if(!R.transport_item && R.stockpile_amount >= R.stockpile_limit)
+				if(message)
+					say("The Crown's [R.name] stockpile is full. Take it elsewhere.")
+				return
+			R.refresh_auto_price()
 			var/amt = R.get_payout_price(I)
-			var/nopay = !R.transport_item && R.held_items[stockpile_index] >= R.stockpile_limit // Check whether it is overflowed BEFORE nopaying them
+			var/true_value = I.get_real_price()
 			if(!R.transport_item)
-				R.held_items[stockpile_index] += 1 //stacked logs need to check for multiple
+				R.stockpile_amount += 1 //stacked logs need to check for multiple
 				qdel(I)
 				if(message == TRUE)
 					stock_announce("[R.name] has been stockpiled.")
@@ -197,17 +263,16 @@
 				if(sound == TRUE)
 					playsound(loc, 'sound/misc/hiss.ogg', 100, FALSE, -1)
 					playsound(loc, 'sound/misc/disposalflush.ogg', 100, FALSE, -1)
-			var/true_value = I.get_real_price()
-			if(nopay)
-				SStreasury.economic_output += true_value // Still count as economic output hah
-				say("Stockpile is full, no payment.")
-			else if(amt)
+			if(amt)
 				SStreasury.economic_output += true_value
-				if(!SStreasury.give_money_account(amt, H, "+[amt] from [R.name] bounty") && message == TRUE)
-					say("No account found. Submit your fingers to a Meister for inspection.")
+				SStreasury.give_money_account(amt, H, "+[amt] from [R.name] bounty")
 			record_round_statistic(STATS_STOCKPILE_EXPANSES, amt) // Unlike deposit, a treasure minting is equal to both expending and profiting at the same time
 			record_round_statistic(STATS_STOCKPILE_REVENUE, true_value)
 			return
+
+	// Nothing in the stockpile accepted this item
+	if(message)
+		say("[I.name] is not accepted here.")
 
 /obj/structure/roguemachine/stockpile/attackby(obj/item/P, mob/user, params)
 	if(istype(P, /obj/item/roguecoin/aalloy))